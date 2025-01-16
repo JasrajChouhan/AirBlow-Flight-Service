73d64e42c97aa59a6d5ec041aad22e06b0d7f2cd
JasrajChouhan <jasrajc945@gmail.com>
2025-01-16 11:27:34 +0530

    (fix): option on prisma-client

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -3,11 +3,6 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient({
   log: ['query'],
   errorFormat: 'pretty',
-  errorFormatOptions: {
-    code: true,
-    message: true,
-    stack: true,
-  },
 });
 
 export default prisma;
